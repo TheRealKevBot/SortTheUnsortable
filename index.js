6d1888238c992dd89d21b4bc353c3f5586b8fc6d
TheRealKevBot <Kglas001@gmail.com>
2021-08-13 12:38:04 -0600

    First

---
+++ index.js
@@ -0,0 +1,16 @@
+//  Create a function that, given an array similar to the above, 
+//  sorts the array according to the "content of the elements".
+
+
+
+function sortIt(arr) {
+	
+} 
+
+console.log(sortIt([4, 1, 3]), [1, 3, 4])
+console.log(sortIt([[4], [1], [3]]), [[1], [3], [4]])
+console.log(sortIt([4, [1], 3]), [[1], 3, 4])
+console.log(sortIt([[4], 1, [3]]), [1, [3], [4]])
+console.log(sortIt([[3], 4, [2], [5], 1, 6]), [1, [2], [3], 4, [5], 6])
+console.log(sortIt([[3], 7, [9], [5], 1, 6]), [1, [3], [5], 6, 7, [9]])
+console.log(sortIt([[3], 7, [9], [5], 1, 6, [0]]), [[0], 1, [3], [5], 6, 7, [9]])
